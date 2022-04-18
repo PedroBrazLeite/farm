ed7c5203c830508a17e1b029ff41a5b37561d07e
PedroBrazLeite <pedrobrazleite@gmail.com>
2022-04-18 11:35:06 -0300

    primeiro teste

---
+++ main.js
@@ -0,0 +1,35 @@
+import './style.css';
+
+import * as THREE from 'three';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var loader = new GLTFLoader();
+
+var obj;
+loader.load('plants.gltf', function (gltf) {
+	obj = gltf.scene;
+	scene.add(gltf.scene);
+});
+scene.background = new THREE.Color(0xffffff);
+var light = new THREE.DirectionalLight(0xffffff, 2);
+scene.add(light);
+camera.position.set(0, 0.5, 3);
+function animate() {
+	requestAnimationFrame(animate);
+	obj.rotation.y += 0.01;
+	renderer.render(scene, camera);
+}
+animate();
