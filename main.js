0b557b18bdeeb64528bf72ede7050972ab5ca3de
PedroBrazLeite <pedrobrazleite@gmail.com>
2022-04-18 13:08:40 -0300

    farm test compleate

--- main.js
+++ main.js
@@ -19,14 +19,24 @@ document.body.appendChild(renderer.domElement);
 var loader = new GLTFLoader();
 
 var obj;
-loader.load('plants.gltf', function (gltf) {
+loader.load('farm.gltf', function (gltf) {
 	obj = gltf.scene;
 	scene.add(gltf.scene);
 });
 scene.background = new THREE.Color(0xffffff);
-var light = new THREE.DirectionalLight(0xffffff, 2);
+
+var light = new THREE.PointLight(0xffffff, 1);
+light.position.set(-8, 0, 12);
+var light2 = new THREE.PointLight(0xffffff, 1);
+light2.position.set(5, -10, 8);
+var light3 = new THREE.PointLight(0xffffff, 1);
+light3.position.set(0, 13, 16);
+
 scene.add(light);
-camera.position.set(0, 0.5, 3);
+scene.add(light2);
+scene.add(light3);
+
+camera.position.set(0, 2.5, 10);
 function animate() {
 	requestAnimationFrame(animate);
 	obj.rotation.y += 0.01;
